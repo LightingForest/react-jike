8ffabd99d97d0b794ea0d50c18186894fb7d792e
Robin_ <3045351695@qq.com>
2024-08-02 23:23:25 +0800

    "登录静态文件导入"

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,29 @@
-const Login=()=>{
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
     return (
-        <div>
-            this is login
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         </div>
     )
 }
-export default Login;
+
+export default Login
