8c4b1b7b934ea3a44cde80b8b870d76fd3e0e8eb
Robin_ <3045351695@qq.com>
2024-08-03 20:04:52 +0800

    "获取表单数据"

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,12 +3,15 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+    const onFinish=(values)=>{
+        console.log(values)
+    }
     return (
         <div className="login">
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt="" />
                 {/* 登录表单 */}
-                <Form validateTrigger="onBlur">
+                <Form onFinish={onFinish} validateTrigger="onBlur">
                     <Form.Item
                         name="mobile"
                         //多条检验逻辑
