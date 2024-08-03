d5d36321664d3aeeb84fc5bf9bf32e691618a53d
Robin_ <3045351695@qq.com>
2024-08-03 20:15:47 +0800

    "axios配置"

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+//统一中转工具块函数
+import {request} from "@/utils/request";
+export {
+    request
+}
---
+++ src/utils/request.js
@@ -0,0 +1,32 @@
+//axios的封装处理
+import axios from "axios";
+
+//1.根域名配置
+//2.超时时间
+//3.请求拦截/响应拦截器
+const request= axios.create({
+    baseURL:'http://geek.itheima.net/v1_0',
+    timeout:5000
+})
+
+// 添加请求拦截器
+//在请求发送之前 做拦截 插入一些自定义的配置[参数的处理]
+request.interceptors.request.use((config)=> {
+    return config
+}, (error)=> {
+    return Promise.reject(error)
+})
+
+// 添加响应拦截器
+//重点处理返回的数据
+request.interceptors.response.use((response)=> {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data
+}, (error)=> {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+})
+
+export {request}
