e013850e8b508f29423d85e93abe2a176419975d
Robin_ <3045351695@qq.com>
2024-08-03 19:59:51 +0800

    "表单检验实现"

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,11 +8,28 @@ const Login = () => {
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt="" />
                 {/* 登录表单 */}
-                <Form>
-                    <Form.Item>
+                <Form validateTrigger="onBlur">
+                    <Form.Item
+                        name="mobile"
+                        //多条检验逻辑
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入手机号!'
+                            },
+                            {
+                                pattern:/^1[3-9]\d{9}$/,
+                                message:'请输入正确的手机号!'
+                            }
+
+                        ]}
+                    >
                         <Input size="large" placeholder="请输入手机号" />
                     </Form.Item>
-                    <Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[{ required: true, message: '请输入验证码!' }]}
+                    >
                         <Input size="large" placeholder="请输入验证码" />
                     </Form.Item>
                     <Form.Item>
